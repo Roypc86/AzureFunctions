27aab021bc4037509f63dd5b2bc56c8148d384aa
Roypc86 <roypc86@gmail.com>
2023-06-21 14:34:39 -0600

    Reorganizing repository

--- AzureFunctionDemo/AzureFunctions1/AzureFunctions1/HttpExample.cs
+++ AzureFunctionDemo/AzureFunctions1/AzureFunctions1/HttpExample.cs
@@ -1,12 +1,11 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace AzureFunctions1
 {
@@ -15,6 +14,7 @@ namespace AzureFunctions1
         [FunctionName("HttpExampleFunction")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            [Queue("outqueue"), StorageAccount("AzureWebJobsStorage")] ICollector<string> msg,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
@@ -24,6 +24,12 @@ namespace AzureFunctions1
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             dynamic data = JsonConvert.DeserializeObject(requestBody);
             name = name ?? data?.name;
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                // Add a message to the output collection.
+                msg.Add(name);
+            }
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
